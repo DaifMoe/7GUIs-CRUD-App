6f466704af2f0b08f86a3baa8c366fd777097315
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-27 14:38:28 -0400

    Changed to CreateEmployee and created EditEmployee

---
+++ src/EditEmployee.tsx
@@ -0,0 +1,5 @@
+const EditEmployee = () => {
+  return <div></div>;
+};
+
+export default EditEmployee;
--- src/EmployeeCreation.tsx
+++
@@ -1,9 +0,0 @@
-const EmployeeCreation = () => {
-    return ( 
-        <div>
-            
-        </div>
-     );
-}
- 
-export default EmployeeCreation;
