a0c6ed5f2a263ae252f29bfc887bfebec867263b
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-27 14:33:52 -0400

    Created db.json and Employee Listing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmployeeListing from "./EmployeeListing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React JS CRUD App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmployeeListing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/EmployeeListing.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+const EmployeeListing = () => {
+  const [employeeData, setEmployeeData] = useState<Employee[] | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/employee")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        console.log(resp);
+        setEmployeeData(resp); // Set the fetched data in state
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>Employee Listings</h2>
+        </div>
+        <div className="card-body">
+          <div>
+            <Link to="" className="btn btn-success">
+              Add New (+)
+            </Link>
+          </div>
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {employeeData &&
+                employeeData.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>
+                      <a className="btn btn-success">Edit</a>
+                      <a className="btn btn-danger">Delete</a>
+                      <a className="btn btn-primary">Details</a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeListing;
